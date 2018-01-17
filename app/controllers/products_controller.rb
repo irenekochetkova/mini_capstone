3b18357435f5a04fe495962a3125c744ddf2693d
Irina <irenekochetkova@gmail.com>
2018-01-16 23:34:56 -0800

    added to route.rb right letter

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,5 @@
 class ProductsController < ApplicationController
 
-  # def single_product_method
-  #   product = Product.first
-  #   render json: product.as_json
-  # end
-
   def all_products
     products = Product.all
     render json: products.as_json
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  # get "/single_product" => "product#single_product_method"
   get "/all_products" => "products#all_products"
 end
