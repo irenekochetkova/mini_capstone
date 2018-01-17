149464af0d90fb347a8e66f8d79768b004469296
Irina <irenekochetkova@gmail.com>
2018-01-16 23:05:56 -0800

    Add created a route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,12 @@
 class ProductsController < ApplicationController
+
+  # def single_product_method
+  #   product = Product.first
+  #   render json: product.as_json
+  # end
+
+  def all_products
+    products = Product.all
+    render json: products.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get "/single_product" => "product#single_product_method"
+  get "/all_products" => "product#all_products"
 end
