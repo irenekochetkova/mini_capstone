eeb15f404f394b559a9472a77de6db6d3ad6234e
Irina <irenekochetkova@gmail.com>
2018-02-01 21:21:20 -0800

    added associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,11 @@ class Product < ApplicationRecord
 
   has_many :category_products 
   has_many :categories, through: :category_products
-  has_many :oders
+  has_many :carted_products
+  has_many :orders, through: :carted_products
+
   belongs_to :supplier
+
 
   validates :title, presence: true, uniqueness: true, length: {minimum: 2}
   validates :price, presence: true
