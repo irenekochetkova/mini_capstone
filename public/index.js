9b72f67774b2a04fd92568babcc1a9277695d2dd
Irina <irenekochetkova@gmail.com>
2018-02-13 19:28:29 -0800

    added index.js

---
+++ public/index.js
@@ -0,0 +1,8 @@
+var productTemplate = document.querySelector('#product-card');
+var productDeck = document.querySelector(".card-deck");
+
+productDeck.appendChild(productTemplate.content.cloneNode(true));
+productDeck.appendChild(productTemplate.content.cloneNode(true));
+productDeck.appendChild(productTemplate.content.cloneNode(true));
+productDeck.appendChild(productTemplate.content.cloneNode(true));
+productDeck.appendChild(productTemplate.content.cloneNode(true));
