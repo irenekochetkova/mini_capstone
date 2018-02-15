6da134b37db16772fc76cf68cfbfa6556e0951c1
Irina <irenekochetkova@gmail.com>
2018-02-14 19:02:56 -0800

    commit update javascript

--- public/index.js
+++ public/index.js
@@ -1,8 +1,25 @@
 var productTemplate = document.querySelector('#product-card');
-var productDeck = document.querySelector(".card-deck");
+var productRow = document.querySelector(".row");
 
-productDeck.appendChild(productTemplate.content.cloneNode(true));
-productDeck.appendChild(productTemplate.content.cloneNode(true));
-productDeck.appendChild(productTemplate.content.cloneNode(true));
-productDeck.appendChild(productTemplate.content.cloneNode(true));
-productDeck.appendChild(productTemplate.content.cloneNode(true));
+// productDeck.appendChild(productTemplate.content.cloneNode(true));
+// productDeck.appendChild(productTemplate.content.cloneNode(true));
+// productDeck.appendChild(productTemplate.content.cloneNode(true));
+// productDeck.appendChild(productTemplate.content.cloneNode(true));
+// productDeck.appendChild(productTemplate.content.cloneNode(true));
+
+axios.get("http://localhost:3000/products").then(function(response){
+  var products = response.data;
+  console.log(response.data);
+
+  products.forEach(function(product){
+    var productCard = productTemplate.content.cloneNode(true);
+    productCard.querySelector(".card-title").innerText = product.title;
+    productCard.querySelector(".card-img-top").src = product.image;
+    productCard.querySelector(".price").innerText = product.price;
+    productCard.querySelector(".description").innerText = product.description;
+    productCard.querySelector(".supplier").innerText = product.supplier;
+
+
+    productRow.appendChild(productCard);
+  }) 
+})
