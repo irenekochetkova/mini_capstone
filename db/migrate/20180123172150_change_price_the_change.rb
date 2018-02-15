b5e1ad1144deedc0b7003aec3ec248597b58526f
Irina <irenekochetkova@gmail.com>
2018-02-14 19:43:45 -0800

    price migration change

--- db/migrate/20180123172150_change_price_the_change.rb
+++ db/migrate/20180123172150_change_price_the_change.rb
@@ -1,5 +1,6 @@
 class ChangePriceTheChange < ActiveRecord::Migration[5.1]
   def change
+    change_column :products, :price, "USING price::integer"
     change_column :products, :price, :integer
   end
 end
