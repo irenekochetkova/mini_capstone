04244f5bb68b7c1d9b55d5c85a4e513210ecd387
Irina <irenekochetkova@gmail.com>
2018-02-16 21:49:45 -0800

    changed data

--- public/index.js
+++ public/index.js
@@ -1,13 +1,11 @@
 var productTemplate = document.querySelector('#product-card');
 var productRow = document.querySelector(".row");
 
-// productDeck.appendChild(productTemplate.content.cloneNode(true));
-// productDeck.appendChild(productTemplate.content.cloneNode(true));
-// productDeck.appendChild(productTemplate.content.cloneNode(true));
-// productDeck.appendChild(productTemplate.content.cloneNode(true));
-// productDeck.appendChild(productTemplate.content.cloneNode(true));
+
 
 axios.get("https://obscure-taiga-94094.herokuapp.com/products").then(function(response){
+
+
   var products = response.data;
   console.log(response.data);
 
